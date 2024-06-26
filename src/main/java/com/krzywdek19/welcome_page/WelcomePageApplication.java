d22450ce9daa3842d2e5ed5f3b54b60901ac6b8c
krzywdek19 <krzywdun@wp.pl>
2024-06-26 18:20:21 +0200

    init app

---
+++ src/main/java/com/krzywdek19/welcome_page/WelcomePageApplication.java
@@ -0,0 +1,13 @@
+package com.krzywdek19.welcome_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomePageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krzywdek19/welcome_page/WelcomePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krzywdek19.welcome_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
